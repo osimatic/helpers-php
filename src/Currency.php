fde658e7b6702bec04e683e24b7b727ce4f87e65
Benoit <guiraudou@osimatic.com>
2020-03-24 10:00:21 +0100

    ajout fonction affichage dans classe Currency

--- src/Currency.php
+++ src/Currency.php
@@ -4,6 +4,10 @@ namespace Osimatic\Helpers;
 
 class Currency
 {
+	/**
+	 * @param string $countryCode
+	 * @return string
+	 */
 	public static function getCurrencyOfCountry(string $countryCode): string
 	{
 		return (new \NumberFormatter(
@@ -11,4 +15,32 @@ class Currency
 			\NumberFormatter::CURRENCY
 		))->getTextAttribute(\NumberFormatter::CURRENCY_CODE);
 	}
+
+	/**
+	 * @param $number
+	 * @param string $currency
+	 * @param int $decimals
+	 * @return string
+	 */
+	public static function format($number, string $currency, int $decimals=2): string
+	{
+		$fmt = new \NumberFormatter(\Locale::getDefault(), \NumberFormatter::CURRENCY);
+		$fmt->setTextAttribute(\NumberFormatter::CURRENCY_CODE, $currency);
+		$fmt->setAttribute(\NumberFormatter::FRACTION_DIGITS, $decimals);
+		return $fmt->formatCurrency($number, $currency);
+	}
+
+	/**
+	 * @param $number
+	 * @param string $currency
+	 * @param int $decimals
+	 * @return string
+	 */
+	public static function formatWithCode($number, string $currency, int $decimals=2)
+	{
+		$fmt = new \NumberFormatter(\Locale::getDefault(), \NumberFormatter::DECIMAL);
+		$fmt->setAttribute(\NumberFormatter::FRACTION_DIGITS, $decimals);
+		return $fmt->format($number).' '.$currency;
+	}
+
 }
