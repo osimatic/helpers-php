64ad80de1eadfa089c1b78ffc0f5017678f5ecb5
Benoit <guiraudou@osimatic.com>
2020-03-24 15:25:55 +0100

    ajout classe Number

---
+++ src/Number.php
@@ -0,0 +1,135 @@
+<?php
+
+namespace Osimatic\Helpers;
+
+class Number
+{
+	/**
+	 * @param mixed $number
+	 * @param int $nbDigitMin
+	 * @param string|null $thousandsSeparator
+	 * @return string
+	 */
+	public static function addLeadingZero($number, int $nbDigitMin, ?string $thousandsSeparator=null): string
+	{
+		$tmp = str_replace((is_null($thousandsSeparator)?'.':$thousandsSeparator), '', $number);
+		$nbLeadingZero = $nbDigitMin-(strlen((string) (int) $tmp));
+		return str_pad($number, $nbLeadingZero, '0', STR_PAD_LEFT);
+	}
+
+	// ========== Arrondissement d'un nombre ==========
+
+	/**
+	 * Arrondi un flottant au plus petit flottant supérieur
+	 * @param float $nombre le flottant à arrondir
+	 * @param int $precision le nombre de chiffre après la virgule à garder
+	 * @return float le flottant arrondi au flottant supérieur
+	 * @link http://fr2.php.net/manual/fr/function.pow.php
+	 * @link http://fr2.php.net/manual/fr/function.ceil.php
+	 */
+	public static function floatRoundUp(float $nombre, int $precision=2): float
+	{
+		if (self::getNbDigitsOfInt(self::decimalPart($nombre)) === $precision) {
+			return $nombre;
+		}
+
+		$e = pow(10, $precision);
+		return ceil($e * $nombre)/$e;
+	}
+
+	/**
+	 * Arrondi un flottant au plus grand flottant inférieur
+	 * @param float $nombre le flottant à arrondir
+	 * @param int $precision le nombre de chiffre après la virgule à garder
+	 * @return float le flottant arrondi au flottant inférieur
+	 * @link http://fr2.php.net/manual/fr/function.pow.php
+	 * @link http://fr2.php.net/manual/fr/function.floor.php
+	 */
+	public static function floatRoundDown(float $nombre, int $precision=2): float
+	{
+		if (self::getNbDigitsOfInt(self::decimalPart($nombre)) === $precision) {
+			return $nombre;
+		}
+
+		$e = pow(10, $precision);
+		return floor($e * $nombre)/$e;
+	}
+
+	// ========== Type / Composition d'un nombre ==========
+
+	/**
+	 * @param $val
+	 * @return bool
+	 */
+	public static function isInteger($val): bool
+	{
+		return ($val - round($val) === 0);
+	}
+
+	/**
+	 * @param $val
+	 * @return bool
+	 */
+	public static function isFloat($val): bool
+	{
+		return ($val - round($val) !== 0);
+	}
+
+	/**
+	 * Calcule le nombre de chiffres d'un entier.
+	 * @example cette fonction retourne l'entier 6 pour le nombre 112233
+	 * @param int|float $int l'entier pour lequel son nombre de chiffres est calculé
+	 * @return int le nombre de chiffres de l'entier
+	 */
+	public static function getNbDigitsOfInt($int) {
+		return strlen((string) (int) $int);
+	}
+
+	// ========== Mathématiques ==========
+
+	/**
+	 * Récupère la valeur numérique décimale d'un nombre sous forme d'entier ou de flottant
+	 * @example cette fonction retourne l'entier 3344 pour le nombre 1122.3344 (ou le flottant 0.3344 si le paramètre $asFloat vaut true
+	 * @param int|float $float le nombre pour lequel la partie décimale est récupérée
+	 * @param boolean $asFloat true pour retourner la valeur numérique décimale du nombre sous forme d'entier, false pour la retourner sous forme de flottant
+	 * @return int|float la partie décimale du nombre, sous forme d'entier
+	 */
+	public static function decimalPart($float, bool $asFloat=false)
+	{
+		if (!self::isFloat($float)) {
+			return 0;
+		}
+		$tabFloat = explode('.', $float);
+
+		if ($asFloat) {
+			return (float) ('0.' . $tabFloat[1]);
+		}
+		return (int) $tabFloat[1];
+	}
+
+	/**
+	 * @param $number
+	 * @return bool
+	 */
+	public static function checkLuhn($number): bool
+	{
+		$somme = 0;
+		$strNumber = (string) $number;
+		$nbDigits = strlen($strNumber);
+		$parity = $nbDigits%2;
+
+		for ($i=($nbDigits-1); $i>=0; $i--) {
+			$digit = $strNumber{$i};
+
+			if ($i%2 == $parity) {
+				$digit = $digit * 2;
+			}
+			if ($digit > 9) {
+				$digit -= 9;
+			}
+			$somme += $digit;
+		}
+		return ($somme % 10) === 0;
+	}
+
+}
