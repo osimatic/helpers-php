e9701eaf11cac8cde517add27306b3c148993281
Benoit <guiraudou@osimatic.com>
2020-03-25 09:55:15 +0100

    ajout fonction Bank::checkIban

--- src/Location/GeographicCoordinates.php
+++ src/Location/GeographicCoordinates.php
@@ -13,29 +13,4 @@ class GeographicCoordinates
 		return preg_match('/^[-+]?([1-8]?\d(\.\d+)?|90(\.0+)?),\s*[-+]?(180(\.0+)?|((1[0-7]\d)|([1-9]?\d))(\.\d+)?)$/', $coordinates);
 	}
 
-	/**
-	 * @param string $originCoordinates
-	 * @param string $destinationCoordinates
-	 * @return float
-	 */
-	public static function getDistance(string $originCoordinates, string $destinationCoordinates): float
-	{
-		[$originLatitude, $originLongitude] = explode(',', $originCoordinates);
-		[$destinationLatitude, $destinationLongitude] = explode(',', $destinationCoordinates);
-		return self::getDistanceBetweenLatitudeAndLongitudeData($originLatitude, $originLongitude, $destinationLatitude, $destinationLongitude);
-	}
-
-	/**
-	 * Retourne distance en mètre
-	 * @param float $originLatitude
-	 * @param float $originLongitude
-	 * @param float $destinationLatitude
-	 * @param float $destinationLongitude
-	 * @return float
-	 */
-	public static function getDistanceBetweenLatitudeAndLongitudeData(float $originLatitude, float $originLongitude, float $destinationLatitude, float $destinationLongitude): float
-	{
-		return sqrt(pow(($destinationLatitude- $originLatitude), 2) + pow(($destinationLongitude-$originLongitude), 2)) * 100000;
-	}
-
 }
