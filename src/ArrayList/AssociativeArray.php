8f37073569e22db617de91e3d04791c31864dd2d
Benoit <guiraudou@osimatic.com>
2020-03-31 09:43:29 +0200

    Ajout classe AssociativeArray

---
+++ src/ArrayList/AssociativeArray.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace Osimatic\Helpers\ArrayList;
+
+class AssociativeArray
+{
+	/**
+	 * @param array $words
+	 * @return array
+	 */
+	public static function getAllCombinations(array $words): array
+	{
+		if ( count($words) <= 1 ) {
+			$result = $words;
+		}
+		else {
+			$result = array();
+			for ( $i = 0; $i < count($words); ++$i ) {
+				$firstword = $words[$i];
+				$remainingwords = array();
+				for ( $j = 0; $j < count($words); ++$j ) {
+					if ( $i <> $j ) $remainingwords[] = $words[$j];
+				}
+				$combos = wordcombos($remainingwords);
+				for ( $j = 0; $j < count($combos); ++$j ) {
+					$result[] = $firstword . ' ' . $combos[$j];
+				}
+			}
+		}
+		return $result;
+	}
+
+}
