8f6e6ea64fca7aa9c3cbf25bed5ea3aebfafff03
Benoit <guiraudou@osimatic.com>
2020-04-21 13:52:04 +0200

    ajout fonction Time::formatHour()

--- src/DateTime/Time.php
+++ src/DateTime/Time.php
@@ -4,5 +4,13 @@ namespace Osimatic\Helpers\DateTime;
 
 class Time
 {
+	/**
+	 * @param int $hour
+	 * @return string
+	 */
+	public static function formatHour(int $hour): string
+	{
+		return sprintf('%02d', $hour).'h';
+	}
 
 }
