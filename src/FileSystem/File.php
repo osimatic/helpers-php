b0baa90c2b1562d71b2357caf12995392645417a
Benoit <guiraudou@osimatic.com>
2020-04-30 09:57:59 +0200

    Ajout fonctions File::getMimeTypesForFile/Extension et File::mb_pathinfo

--- src/FileSystem/File.php
+++ src/FileSystem/File.php
@@ -65,4 +65,173 @@ class File
 
 		return $float.' '.$units[$e];
 	}
+
+	/**
+	 * Map a file name to a MIME type.
+	 * Defaults to 'application/octet-stream', i.e.. arbitrary binary data.
+	 * @param string $filename A file name or full path, does not need to exist as a file
+	 * @return string
+	 */
+	public static function getMimeTypesForFile($filename): string
+	{
+		// In case the path is a Bitly, strip any query string before getting extension
+		$qpos = strpos($filename, '?');
+		if (false !== $qpos) {
+			$filename = substr($filename, 0, $qpos);
+		}
+
+		return self::getMimeTypesForFileExtension(self::mb_pathinfo($filename, PATHINFO_EXTENSION));
+	}
+
+	/**
+	 * Get the MIME type for a file extension.
+	 * @param string $ext File extension
+	 * @return string MIME type of file.
+	 */
+	public static function getMimeTypesForFileExtension(string $ext = ''): string
+	{
+		$mimes = [
+			'xl' 	=> 'application/excel',
+			'js' 	=> 'application/javascript',
+			'hqx' 	=> 'application/mac-binhex40',
+			'cpt' 	=> 'application/mac-compactpro',
+			'bin' 	=> 'application/macbinary',
+			'doc' 	=> 'application/msword',
+			'word' 	=> 'application/msword',
+			'xlsx' 	=> 'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet',
+			'xltx' 	=> 'application/vnd.openxmlformats-officedocument.spreadsheetml.template',
+			'potx' 	=> 'application/vnd.openxmlformats-officedocument.presentationml.template',
+			'ppsx' 	=> 'application/vnd.openxmlformats-officedocument.presentationml.slideshow',
+			'pptx' 	=> 'application/vnd.openxmlformats-officedocument.presentationml.presentation',
+			'sldx' 	=> 'application/vnd.openxmlformats-officedocument.presentationml.slide',
+			'docx' 	=> 'application/vnd.openxmlformats-officedocument.wordprocessingml.document',
+			'dotx' 	=> 'application/vnd.openxmlformats-officedocument.wordprocessingml.template',
+			'xlam' 	=> 'application/vnd.ms-excel.addin.macroEnabled.12',
+			'xlsb' 	=> 'application/vnd.ms-excel.sheet.binary.macroEnabled.12',
+			'class' => 'application/octet-stream',
+			'dll' 	=> 'application/octet-stream',
+			'dms' 	=> 'application/octet-stream',
+			'exe' 	=> 'application/octet-stream',
+			'lha' 	=> 'application/octet-stream',
+			'lzh' 	=> 'application/octet-stream',
+			'psd' 	=> 'application/octet-stream',
+			'sea' 	=> 'application/octet-stream',
+			'so' 	=> 'application/octet-stream',
+			'oda' 	=> 'application/oda',
+			'pdf' 	=> 'application/pdf',
+			'ai' 	=> 'application/postscript',
+			'eps' 	=> 'application/postscript',
+			'ps' 	=> 'application/postscript',
+			'smi' 	=> 'application/smil',
+			'smil' 	=> 'application/smil',
+			'mif' 	=> 'application/vnd.mif',
+			'xls' 	=> 'application/vnd.ms-excel',
+			'ppt' 	=> 'application/vnd.ms-powerpoint',
+			'wbxml' => 'application/vnd.wap.wbxml',
+			'wmlc' 	=> 'application/vnd.wap.wmlc',
+			'dcr' 	=> 'application/x-director',
+			'dir' 	=> 'application/x-director',
+			'dxr' 	=> 'application/x-director',
+			'dvi' 	=> 'application/x-dvi',
+			'gtar' 	=> 'application/x-gtar',
+			'php3' 	=> 'application/x-httpd-php',
+			'php4' 	=> 'application/x-httpd-php',
+			'php' 	=> 'application/x-httpd-php',
+			'phtml' => 'application/x-httpd-php',
+			'phps' 	=> 'application/x-httpd-php-source',
+			'swf' 	=> 'application/x-shockwave-flash',
+			'sit' 	=> 'application/x-stuffit',
+			'tar' 	=> 'application/x-tar',
+			'tgz' 	=> 'application/x-tar',
+			'xht' 	=> 'application/xhtml+xml',
+			'xhtml' => 'application/xhtml+xml',
+			'zip' 	=> 'application/zip',
+			'mid' 	=> 'audio/midi',
+			'midi' 	=> 'audio/midi',
+			'mp2' 	=> 'audio/mpeg',
+			'mp3' 	=> 'audio/mpeg',
+			'mpga' 	=> 'audio/mpeg',
+			'aif' 	=> 'audio/x-aiff',
+			'aifc' 	=> 'audio/x-aiff',
+			'aiff' 	=> 'audio/x-aiff',
+			'ram' 	=> 'audio/x-pn-realaudio',
+			'rm' 	=> 'audio/x-pn-realaudio',
+			'rpm' 	=> 'audio/x-pn-realaudio-plugin',
+			'ra' 	=> 'audio/x-realaudio',
+			'wav' 	=> 'audio/x-wav',
+			'bmp' 	=> 'image/bmp',
+			'gif' 	=> 'image/gif',
+			'jpeg' 	=> 'image/jpeg',
+			'jpe' 	=> 'image/jpeg',
+			'jpg' 	=> 'image/jpeg',
+			'png' 	=> 'image/png',
+			'tiff' 	=> 'image/tiff',
+			'tif' 	=> 'image/tiff',
+			'eml' 	=> 'message/rfc822',
+			'css' 	=> 'text/css',
+			'html' 	=> 'text/html',
+			'htm' 	=> 'text/html',
+			'shtml' => 'text/html',
+			'log' 	=> 'text/plain',
+			'text' 	=> 'text/plain',
+			'txt' 	=> 'text/plain',
+			'rtx' 	=> 'text/richtext',
+			'rtf' 	=> 'text/rtf',
+			'vcf' 	=> 'text/vcard',
+			'vcard' => 'text/vcard',
+			'xml' 	=> 'text/xml',
+			'xsl' 	=> 'text/xml',
+			'mpeg' 	=> 'video/mpeg',
+			'mpe' 	=> 'video/mpeg',
+			'mpg' 	=> 'video/mpeg',
+			'mov' 	=> 'video/quicktime',
+			'qt' 	=> 'video/quicktime',
+			'rv' 	=> 'video/vnd.rn-realvideo',
+			'avi' 	=> 'video/x-msvideo',
+			'movie' => 'video/x-sgi-movie'
+		];
+		return $mimes[strtolower($ext)] ?? 'application/octet-stream';
+	}
+
+	/**
+	 * Multi-byte-safe pathinfo replacement.
+	 * Drop-in replacement for pathinfo(), but multibyte-safe, cross-platform-safe, old-version-safe.
+	 * Works similarly to the one in PHP >= 5.2.0
+	 * @link http://www.php.net/manual/en/function.pathinfo.php#107461
+	 * @param string $path A filename or path, does not need to exist as a file
+	 * @param integer $options Either a PATHINFO_* constant
+	 * @return string|array
+	 */
+	public static function mb_pathinfo(string $path, ?int $options = null)
+	{
+		$ret = ['dirname' => '', 'basename' => '', 'extension' => '', 'filename' => ''];
+		$pathinfo = [];
+		if (preg_match('%^(.*?)[\\\\/]*(([^/\\\\]*?)(\.([^\.\\\\/]+?)|))[\\\\/\.]*$%im', $path, $pathinfo)) {
+			if (array_key_exists(1, $pathinfo)) {
+				$ret['dirname'] = $pathinfo[1];
+			}
+			if (array_key_exists(2, $pathinfo)) {
+				$ret['basename'] = $pathinfo[2];
+			}
+			if (array_key_exists(5, $pathinfo)) {
+				$ret['extension'] = $pathinfo[5];
+			}
+			if (array_key_exists(3, $pathinfo)) {
+				$ret['filename'] = $pathinfo[3];
+			}
+		}
+		switch ($options) {
+			case PATHINFO_DIRNAME:
+				return $ret['dirname'];
+			case PATHINFO_BASENAME:
+				return $ret['basename'];
+			case PATHINFO_EXTENSION:
+				return $ret['extension'];
+			case PATHINFO_FILENAME:
+				return $ret['filename'];
+			default:
+				return $ret;
+		}
+	}
+
 }
