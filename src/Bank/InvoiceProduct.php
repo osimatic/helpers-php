8cc0b061368f3b864fd7c124c1ff47d15cab6f1b
Benoit <guiraudou@osimatic.com>
2020-06-25 18:37:35 +0200

    ajout classe InvoiceProduct

---
+++ src/Bank/InvoiceProduct.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace Osimatic\Helpers\Bank;
+
+class InvoiceProduct
+{
+	protected float $unitPrice;
+	protected float $quantity;
+
+	/**
+	 * @return float
+	 */
+	public function getUnitPrice(): float
+	{
+		return $this->unitPrice;
+	}
+
+	/**
+	 * @param float $unitPrice
+	 */
+	public function setUnitPrice(float $unitPrice): void
+	{
+		$this->unitPrice = $unitPrice;
+	}
+
+	/**
+	 * @return float
+	 */
+	public function getQuantity(): float
+	{
+		return $this->quantity;
+	}
+
+	/**
+	 * @param float $quantity
+	 */
+	public function setQuantity(float $quantity): void
+	{
+		$this->quantity = $quantity;
+	}
+}
