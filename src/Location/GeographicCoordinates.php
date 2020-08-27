87237de4b09d52ba255e7b9d46b14e3130f929f7
Benoit <guiraudou@osimatic.com>
2020-08-27 16:19:59 +0200

    correction bug formattage coordonnées géograpghiques

--- src/Location/GeographicCoordinates.php
+++ src/Location/GeographicCoordinates.php
@@ -13,4 +13,15 @@ class GeographicCoordinates
 		return preg_match('/^[-+]?([1-8]?\d(\.\d+)?|90(\.0+)?),\s*[-+]?(180(\.0+)?|((1[0-7]\d)|([1-9]?\d))(\.\d+)?)$/', $coordinates);
 	}
 
+	/**
+	 * @param float $latitude
+	 * @param float $longitude
+	 * @return string
+	 */
+	public static function getCoordinatesFromLatitudeAndLongitude(float $latitude, float $longitude): string
+	{
+		$cleanLatOrLng = fn(string $latOrLng) => str_replace([' ', ','], ['', '.'], $latOrLng);
+		return $cleanLatOrLng((string) $latitude).','.$cleanLatOrLng((string) $longitude);
+	}
+
 }
