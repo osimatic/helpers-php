af20d90a2cc597c7b9c6c002f8b1cc405eca985a
benoitguiraudou <guiraudou@osimatic.com>
2020-09-08 23:07:30 +0200

    correction bug accent/encodage

--- src/Calendar/Date.php
+++ src/Calendar/Date.php
@@ -8,12 +8,16 @@ class Date
 
 	/**
 	 * @param int $dayOfWeek
+	 * @param string|null $locale
+	 * @see http://userguide.icu-project.org/formatparse/datetime
 	 * @return string
 	 */
-	public static function getDayName(int $dayOfWeek): string
+	public static function getDayName(int $dayOfWeek, ?string $locale=null): string
 	{
-		$timestamp = strtotime('monday this week');
-		return ucfirst(strftime('%A', ($timestamp+($dayOfWeek*3600*24))));
+		$timestamp = strtotime('monday this week')+($dayOfWeek*3600*24);
+		return ucfirst(\IntlDateFormatter::create($locale, \IntlDateFormatter::FULL, \IntlDateFormatter::FULL,
+			date_default_timezone_get(), \IntlDateFormatter::GREGORIAN , 'EEEE')->format($timestamp));
+		//return ucfirst(strftime('%A', ($timestamp+($dayOfWeek*3600*24))));
 	}
 
 	// ========== Jour du mois ==========
@@ -22,11 +26,15 @@ class Date
 
 	/**
 	 * @param int $month
+	 * @param string|null $locale
+	 * @see http://userguide.icu-project.org/formatparse/datetime
 	 * @return string
 	 */
-	public static function getMonthName(int $month): string
+	public static function getMonthName(int $month, ?string $locale=null): string
 	{
-		return ucfirst(strftime('%B', mktime(0, 0, 0, $month)));
+		return ucfirst(\IntlDateFormatter::create($locale, \IntlDateFormatter::FULL, \IntlDateFormatter::FULL,
+			date_default_timezone_get(), \IntlDateFormatter::GREGORIAN , 'MMMM')->format(new \DateTime('2020-'.sprintf('%02d', $month).'-15 00:00:00')));
+		//return ucfirst(strftime('%B', mktime(0, 0, 0, $month)));
 	}
 
 	/**
