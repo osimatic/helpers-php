771f09c0a993c59c7337bca2d0c00f9f72d8097b
Benoit <guiraudou@osimatic.com>
2020-09-09 11:22:58 +0200

    correction bug nom jour de la semaine

--- src/Calendar/Date.php
+++ src/Calendar/Date.php
@@ -7,14 +7,14 @@ class Date
 	// ========== Jour de la semaine ==========
 
 	/**
-	 * @param int $dayOfWeek
+	 * @param int $dayOfWeek ISO-8601 numeric representation of the day of the week : 1 (for Monday) through 7 (for Sunday)
 	 * @param string|null $locale
 	 * @see http://userguide.icu-project.org/formatparse/datetime
 	 * @return string
 	 */
 	public static function getDayName(int $dayOfWeek, ?string $locale=null): string
 	{
-		$timestamp = strtotime('monday this week')+($dayOfWeek*3600*24);
+		$timestamp = strtotime('monday this week')+(($dayOfWeek-1)*3600*24);
 		return ucfirst(\IntlDateFormatter::create($locale, \IntlDateFormatter::FULL, \IntlDateFormatter::FULL,
 			date_default_timezone_get(), \IntlDateFormatter::GREGORIAN , 'EEEE')->format($timestamp));
 		//return ucfirst(strftime('%A', ($timestamp+($dayOfWeek*3600*24))));
@@ -25,7 +25,7 @@ class Date
 	// ========== Mois ==========
 
 	/**
-	 * @param int $month
+	 * @param int $month Numeric representation of a month
 	 * @param string|null $locale
 	 * @see http://userguide.icu-project.org/formatparse/datetime
 	 * @return string
