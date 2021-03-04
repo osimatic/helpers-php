73fa2e9a6f39142ecbdd78b9dccd5810e90d8d4f
Benoit <guiraudou@osimatic.com>
2021-03-04 19:08:16 +0100

    ajout diverses fonction dans class DatePeriod

--- src/Calendar/DatePeriod.php
+++ src/Calendar/DatePeriod.php
@@ -8,6 +8,8 @@ namespace Osimatic\Helpers\Calendar;
  */
 class DatePeriod
 {
+	// ========== Jours ==========
+
 	/**
 	 * @param \DateTime $periodStart
 	 * @param \DateTime $periodEnd
@@ -16,7 +18,7 @@ class DatePeriod
 	public static function getNbDays(\DateTime $periodStart, \DateTime $periodEnd): int
 	{
 		try {
-			$startDate = new \DateTime($periodStart->format('Y-m-d') . ' 00:00:00');
+			$startDate = new \DateTime($periodStart->format('Y-m-d').' 00:00:00');
 			$endDate = new \DateTime($periodEnd->format('Y-m-d').' 00:00:00');
 			return (int) $startDate->diff($endDate)->format('%r%a');
 		} catch (\Exception $e) { }
@@ -39,18 +41,25 @@ class DatePeriod
 	 * @param int[]|null $weekDays
 	 * @return \DateTime[]
 	 */
-	public static function getListDaysOfMonths(\DateTime $periodStart, \DateTime $periodEnd, ?array $weekDays=null): array
+	public static function getListOfDateDaysOfTheMonth(\DateTime $periodStart, \DateTime $periodEnd, ?array $weekDays=null): array
 	{
 		$startIntervalDate = (clone $periodStart)->setTime(0, 0, 0);
 		$endIntervalDate = (clone $periodEnd)->setTime(0, 0, 0)->modify('+1 day');
 
-		$list = [];
-		$dateRange = new \DatePeriod($startIntervalDate, new \DateInterval('P1D'), $endIntervalDate);
-		foreach ($dateRange as $date) {
-			if (null === $weekDays || in_array((int) $date->format('N'), $weekDays, true)) {
-				$list[] = $date;
+		try {
+			$list = [];
+			$dateRange = new \DatePeriod($startIntervalDate, new \DateInterval('P1D'), $endIntervalDate);
+			foreach ($dateRange as $date) {
+				if (null === $weekDays || in_array((int) $date->format('N'), $weekDays, true)) {
+					$list[] = $date;
+				}
 			}
+			return $list;
 		}
+		catch (\Exception $e) {
+			return [];
+		}
+
 		/*
 		for ($timestamp=$periodStart->getTimestamp(); $timestamp<=$periodEnd->getTimestamp(); $timestamp+=86400) {
 			if (null === $weekDays || in_array((int) date('N', $timestamp), $weekDays, true)) {
@@ -61,8 +70,131 @@ class DatePeriod
 			}
 		}
 		*/
-		return $list;
 	}
+
+	/**
+	 * @param \DateTime $periodStart
+	 * @param \DateTime $periodEnd
+	 * @param int[]|null $weekDays
+	 * @param string $dateFormat
+	 * @return string[]
+	 */
+	public static function getListOfDaysOfTheMonth(\DateTime $periodStart, \DateTime $periodEnd, ?array $weekDays=null, string $dateFormat='Y-m-d'): array
+	{
+		$startIntervalDate = (clone $periodStart)->setTime(0, 0, 0);
+		$endIntervalDate = (clone $periodEnd)->setTime(0, 0, 0)->modify('+1 day');
+
+		try {
+			$list = [];
+			$dateRange = new \DatePeriod($startIntervalDate, new \DateInterval('P1D'), $endIntervalDate);
+			foreach ($dateRange as $date) {
+				if (null === $weekDays || in_array((int) $date->format('N'), $weekDays, true)) {
+					$list[] = $date->format($dateFormat);
+				}
+			}
+			return $list;
+		}
+		catch (\Exception $e) {
+			return [];
+		}
+	}
+
+	// ========== Semaines ==========
+
+	/**
+	 * @param \DateTime $periodStart
+	 * @param \DateTime $periodEnd
+	 * @return int
+	 */
+	public static function getNbFullWeeks(\DateTime $periodStart, \DateTime $periodEnd): int
+	{
+		return floor(self::getNbDays($periodStart, $periodEnd) / 7);
+	}
+
+	/**
+	 * @param \DateTime $periodStart
+	 * @param \DateTime $periodEnd
+	 * @return int
+	 */
+	public static function getNbWeeks(\DateTime $periodStart, \DateTime $periodEnd): int
+	{
+		return count(self::getListOfWeeks($periodStart, $periodEnd));
+	}
+
+	/**
+	 * @param \DateTime $periodStart
+	 * @param \DateTime $periodEnd
+	 * @return string[]
+	 */
+	public static function getListOfWeeks(\DateTime $periodStart, \DateTime $periodEnd, string $dateFormat='Y-W'): array
+	{
+		$startIntervalDate = (clone $periodStart)->modify('Monday this week');
+		$endIntervalDate = (clone $periodEnd)->modify('this Sunday');
+
+		try {
+			$dateRange = new \DatePeriod($startIntervalDate, new \DateInterval('P1W'), $endIntervalDate);
+
+			$periodList = [];
+			foreach ($dateRange as $date) {
+				$periodList[] = $date->format($dateFormat);
+			}
+			return $periodList;
+		}
+		catch (\Exception $e) {
+			return [];
+		}
+	}
+
+	// ========== Mois ==========
+
+	/**
+	 * @param \DateTime $periodStart
+	 * @param \DateTime $periodEnd
+	 * @return int
+	 */
+	public static function getNbFullMonths(\DateTime $periodStart, \DateTime $periodEnd): int
+	{
+		// todo
+		return 0;
+	}
+
+	/**
+	 * @param \DateTime $periodStart
+	 * @param \DateTime $periodEnd
+	 * @return int
+	 */
+	public static function getNbMonths(\DateTime $periodStart, \DateTime $periodEnd): int
+	{
+		return count(self::getListOfMonths($periodStart, $periodEnd));
+	}
+
+	/**
+	 * @param \DateTime $periodStart
+	 * @param \DateTime $periodEnd
+	 * @param string $dateFormat
+	 * @return string[]
+	 */
+	public static function getListOfMonths(\DateTime $periodStart, \DateTime $periodEnd, string $dateFormat='Y-n'): array
+	{
+		$startIntervalDate = (clone $periodStart)->modify('first day of this month');
+		$endIntervalDate = (clone $periodEnd)->modify('last day of this month');
+
+		try {
+			$dateRange = new \DatePeriod($startIntervalDate, new \DateInterval('P1M'), $endIntervalDate);
+
+			$periodList = [];
+			foreach ($dateRange as $date) {
+				$periodList[] = $date->format($dateFormat);
+			}
+			return $periodList;
+		}
+		catch (\Exception $e) {
+			return [];
+		}
+	}
+
+
+	// ========== Interval ==========
 
 	/**
 	 * @param \DateTime $periodStart
@@ -107,13 +239,13 @@ class DatePeriod
 			return ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9', '10', '11', '12', '13', '14', '15', '16', '17', '18', '19', '20', '21', '22', '23'];
 		}
 		if ($groupBy === 'day_of_month') {
-			return self::getPeriod($periodStart, $periodEnd, 'P1D', 'Y-m-d');
+			return self::getListOfDaysOfTheMonth($periodStart, $periodEnd, null, 'Y-m-d');
 		}
 		if ($groupBy === 'week') {
-			return self::getPeriod($periodStart, $periodEnd, 'P1W', 'Y-W');
+			return self::getListOfWeeks($periodStart, $periodEnd, 'Y-W');
 		}
 		if ($groupBy === 'month') {
-			return self::getPeriod($periodStart, $periodEnd, 'P1M', 'Y-n');
+			return self::getListOfMonths($periodStart, $periodEnd, 'Y-n');
 		}
 		if ($groupBy === 'day_of_week') {
 			return ['1', '2', '3', '4', '5', '6', '7'];
@@ -122,13 +254,7 @@ class DatePeriod
 		return null;
 	}
 
-	/**
-	 * @param $startDate
-	 * @param $endDate
-	 * @param $dateInterval
-	 * @param $dateFormat
-	 * @return array|null
-	 */
+	/*
 	private static function getPeriod(\DateTime $startDate, \DateTime $endDate, $dateInterval, $dateFormat): ?array
 	{
 		if ($dateInterval === 'P1D') {
@@ -153,17 +279,29 @@ class DatePeriod
 			return null;
 		}
 
-		//var_dump($startIntervalDate->format('Y-m-d'));
-		//var_dump($endIntervalDate->format('Y-m-d'));
-
 		$dateRange = new \DatePeriod($startIntervalDate, $interval, $endIntervalDate);
 		$periodList = [];
 		foreach ($dateRange as $date) {
 			$periodList[] = $date->format($dateFormat);
 		}
 
-		//var_dump($periodList);
 		return $periodList;
+	}
+	*/
+
+
+	// deprecated
+
+	/**
+	 * @param \DateTime $periodStart
+	 * @param \DateTime $periodEnd
+	 * @param array|null $weekDays
+	 * @return \DateTime[]
+	 * @deprecated
+	 */
+	public static function getListDaysOfMonths(\DateTime $periodStart, \DateTime $periodEnd, ?array $weekDays=null): array
+	{
+		return self::getListOfDateDaysOfTheMonth($periodStart, $periodEnd, $weekDays);
 	}
 
 }
