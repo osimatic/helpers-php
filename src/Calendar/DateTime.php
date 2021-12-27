8adbdf36e4e002bafec4e5ac93eb412b2864dbf5
Benoit <guiraudou@osimatic.com>
2021-12-27 15:58:01 +0100

    add function parseFromYearMonthDay

--- src/Calendar/DateTime.php
+++ src/Calendar/DateTime.php
@@ -190,6 +190,22 @@ class DateTime
 		return null;
 	}
 
+	/**
+	 * @param int $year
+	 * @param int $month
+	 * @param int $day
+	 * @return \DateTime|null
+	 */
+	public static function parseFromYearMonthDay(int $year, int $month, int $day): ?\DateTime
+	{
+		try {
+			$d = new \DateTime();
+			$d->setDate($year, $month, $day);
+			return $d;
+		} catch (\Exception $e) {}
+		return null;
+	}
+
 	// ========== Comparaison ==========
 
 	/**
