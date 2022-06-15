c030e4c7fe6f8c9a78b185cdfcfc0e2e5e07908c
Benoit <guiraudou@osimatic.com>
2022-06-15 11:36:50 +0200

    tableau de AccountTransaction à la place de tableau de tableau

--- src/Bank/Accounting.php
+++ src/Bank/Accounting.php
@@ -13,7 +13,7 @@ class Accounting
 	/**
 	 *
 	 */
-	protected $accounts;
+	protected array $accounts = [];
 
 	/**
 	 * @param array $accounts
@@ -27,6 +27,109 @@ class Accounting
 	}
 
 	/**
+	 * @param string $filePath
+	 * @param AccountingTransaction[] $transactionList
+	 */
+	public function generateFile(string $filePath, array $transactionList): void
+	{
+		// Header
+		$tableHead = [
+			'DATES',
+			'JOURNAL',
+			'COMPTES',
+			'NUM FACTURE',
+			'NOMS',
+			'DEBIT (ttc)',
+			'CREDIT (ht et tva)',
+			'CODE MONNAIE',
+		];
+
+		// Liste des transactions
+		$tableBody = [];
+		foreach ($transactionList as $transaction) {
+			$codeMonnaie = $this->getCurrencyCode($transaction->getCurrency());
+			$accountKey = $this->getAccountKey($transaction->getCustomerCountry(), $transaction->getCustomerPostCode(), $transaction->getCustomerVatNumber());
+			$addTvaLine = !empty(BillingTax::getBillingTaxRate($transaction->getCustomerCountry(), $transaction->getCustomerPostCode(), $transaction->getCustomerVatNumber(), 'FR'));
+
+			$transactionDate = $transaction->getDateTime();
+
+			// Ligne débit
+			$account = $transaction->getDebitAccount() ?? $this->accounts[$accountKey]['debit'] ?? '';
+			$tableBody[] = [
+				$transactionDate->format('d/m/Y'),
+				'VT',
+				$account,
+				$transaction->getInvoiceNumber(),
+				utf8_encode($transaction->getCustomerIdentity()),
+				$transaction->getAmountInclTax(),
+				0,
+				$codeMonnaie,
+			];
+
+			// Ligne crédit (HT)
+			$account = $this->accounts[$accountKey]['credit_excl_tax'] ?? '';
+			$tableBody[] = [
+				$transactionDate->format('d/m/Y'),
+				'VT',
+				$account,
+				$transaction->getInvoiceNumber(),
+				utf8_encode($transaction->getCustomerIdentity()),
+				0,
+				$transaction->getAmountExclTax(),
+				$codeMonnaie,
+			];
+
+			// Ligne crédit (TVA)
+			if ($addTvaLine) {
+				$account = $this->accounts[$accountKey]['credit_vat'] ?? '';
+				$tableBody[] = [
+					$transactionDate->format('d/m/Y'),
+					'VT',
+					$account,
+					$transaction->getInvoiceNumber(),
+					utf8_encode($transaction->getCustomerIdentity()),
+					0,
+					$transaction->getAmountVat(),
+					$codeMonnaie,
+				];
+			}
+		}
+
+		\Osimatic\Helpers\Text\CSV::generateFile($filePath, $tableHead, $tableBody, null);
+	}
+
+	private function getAccountKey(?string $customerCountry, ?string $customerZipCode=null, ?string $vatNumber=null): string
+	{
+		if (Country::isCountryInFranceOverseas($customerCountry, $customerZipCode)) {
+			return 'france_dom';
+		}
+		if ('FR' === $customerCountry) {
+			return 'france';
+		}
+		if (Country::isCountryInEuropeanUnion($customerCountry)) {
+			if (empty($vatNumber)) { // Si n° TVA non renseigné, vente assimilé à France (TVA = 20%)
+				return 'france';
+			}
+			return 'inside_ue';
+		}
+		return 'outside_ue';
+	}
+
+	private function getCurrencyCode(string $currency): string
+	{
+		if ('GBP' === $currency) {
+			return 'G';
+		}
+		return 'E';
+	}
+
+
+
+
+
+
+	/**
+	 * @deprecated
 	 * @param $filePath
 	 * @param array $transactionList
 	 */
@@ -106,28 +209,4 @@ class Accounting
 		\Osimatic\Helpers\Text\CSV::generateFile($filePath, $tableHead, $tableBody, null);
 	}
 
-	private function getAccountKey(?string $customerCountry, ?string $customerZipCode=null, ?string $vatNumber=null): string
-	{
-		if (Country::isCountryInFranceOverseas($customerCountry, $customerZipCode)) {
-			return 'france_dom';
-		}
-		if ('FR' === $customerCountry) {
-			return 'france';
-		}
-		if (Country::isCountryInEuropeanUnion($customerCountry)) {
-			if (empty($vatNumber)) { // Si n° TVA non renseigné, vente assimilé à France (TVA = 20%)
-				return 'france';
-			}
-			return 'inside_ue';
-		}
-		return 'outside_ue';
-	}
-
-	private function getCurrencyCode(string $currency): string
-	{
-		if ('GBP' === $currency) {
-			return 'G';
-		}
-		return 'E';
-	}
 }
---
+++ src/Bank/AccountingTransaction.php
@@ -0,0 +1,209 @@
+<?php
+
+namespace Osimatic\Helpers\Bank;
+
+class AccountingTransaction
+{
+	private \DateTime $dateTime;
+	private float $amountExclTax;
+	private float $amountVat;
+	private float $amountInclTax;
+	private string $currency = 'EUR';
+	private string $invoiceNumber;
+	private string $customerIdentity;
+	private ?string $customerPostCode = null;
+	private string $customerCountry = 'FR';
+	private ?string $customerVatNumber = null;
+	private ?string $debitAccount = null;
+
+	/**
+	 * @param string $sqlDate
+	 */
+	public function setSqlDate(string $sqlDate): void
+	{
+		try {
+			$this->dateTime = new \DateTime($sqlDate.' 00:00:00');
+		}
+		catch (\Exception $e) {}
+	}
+
+
+
+	/**
+	 * @return \DateTime
+	 */
+	public function getDateTime(): \DateTime
+	{
+		return $this->dateTime ?? new \DateTime();
+	}
+
+	/**
+	 * @param \DateTime $dateTime
+	 */
+	public function setDateTime(\DateTime $dateTime): void
+	{
+		$this->dateTime = $dateTime;
+	}
+
+	/**
+	 * @return float
+	 */
+	public function getAmountExclTax(): float
+	{
+		return round($this->amountExclTax ?? 0., 2);
+	}
+
+	/**
+	 * @param float $amountExclTax
+	 */
+	public function setAmountExclTax(float $amountExclTax): void
+	{
+		$this->amountExclTax = $amountExclTax;
+	}
+
+	/**
+	 * @return float
+	 */
+	public function getAmountVat(): float
+	{
+		return round($this->amountVat ?? 0., 2);
+	}
+
+	/**
+	 * @param float $amountVat
+	 */
+	public function setAmountVat(float $amountVat): void
+	{
+		$this->amountVat = $amountVat;
+	}
+
+	/**
+	 * @return float
+	 */
+	public function getAmountInclTax(): float
+	{
+		return round($this->amountInclTax ?? 0., 2);
+	}
+
+	/**
+	 * @param float $amountInclTax
+	 */
+	public function setAmountInclTax(float $amountInclTax): void
+	{
+		$this->amountInclTax = $amountInclTax;
+	}
+
+	/**
+	 * @return string
+	 */
+	public function getCurrency(): string
+	{
+		return $this->currency;
+	}
+
+	/**
+	 * @param string $currency
+	 */
+	public function setCurrency(string $currency): void
+	{
+		$this->currency = $currency;
+	}
+
+	/**
+	 * @return string
+	 */
+	public function getInvoiceNumber(): string
+	{
+		return $this->invoiceNumber ?? '';
+	}
+
+	/**
+	 * @param string $invoiceNumber
+	 */
+	public function setInvoiceNumber(string $invoiceNumber): void
+	{
+		$this->invoiceNumber = $invoiceNumber;
+	}
+
+	/**
+	 * @return string
+	 */
+	public function getCustomerIdentity(): string
+	{
+		return $this->customerIdentity ?? '';
+	}
+
+	/**
+	 * @param string $customerIdentity
+	 */
+	public function setCustomerIdentity(string $customerIdentity): void
+	{
+		$this->customerIdentity = $customerIdentity;
+	}
+
+	/**
+	 * @return string|null
+	 */
+	public function getCustomerPostCode(): ?string
+	{
+		return $this->customerPostCode;
+	}
+
+	/**
+	 * @param string|null $customerPostCode
+	 */
+	public function setCustomerPostCode(?string $customerPostCode): void
+	{
+		$this->customerPostCode = $customerPostCode;
+	}
+
+	/**
+	 * @return string
+	 */
+	public function getCustomerCountry(): string
+	{
+		return $this->customerCountry;
+	}
+
+	/**
+	 * @param string $customerCountry
+	 */
+	public function setCustomerCountry(string $customerCountry): void
+	{
+		$this->customerCountry = $customerCountry;
+	}
+
+	/**
+	 * @return string|null
+	 */
+	public function getCustomerVatNumber(): ?string
+	{
+		return $this->customerVatNumber;
+	}
+
+	/**
+	 * @param string|null $customerVatNumber
+	 */
+	public function setCustomerVatNumber(?string $customerVatNumber): void
+	{
+		$this->customerVatNumber = $customerVatNumber;
+	}
+
+	/**
+	 * @return string|null
+	 */
+	public function getDebitAccount(): ?string
+	{
+		return $this->debitAccount;
+	}
+
+	/**
+	 * @param string|null $debitAccount
+	 */
+	public function setDebitAccount(?string $debitAccount): void
+	{
+		$this->debitAccount = $debitAccount;
+	}
+
+
+}
