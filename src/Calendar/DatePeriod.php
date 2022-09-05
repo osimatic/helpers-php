6e95b3264d1f0f41e73d205ee51e845e39722c8c
Benoit <guiraudou@osimatic.com>
2022-09-05 15:12:29 +0200

    ajout fonction isFullWeeks et isFullMonths

--- src/Calendar/DatePeriod.php
+++ src/Calendar/DatePeriod.php
@@ -156,6 +156,16 @@ class DatePeriod
 		return self::getNbDaysBetweenDatesAndTimes($periodStart, $periodEnd) === 6 && ((int) $periodStart->format('N')) === 1 && ((int) $periodEnd->format('N')) === 7;
 	}
 
+	/**
+	 * @param \DateTime $periodStart
+	 * @param \DateTime $periodEnd
+	 * @return bool
+	 */
+	public static function isFullWeeks(\DateTime $periodStart, \DateTime $periodEnd): bool
+	{
+		return ((int) $periodStart->format('N')) === 1 && ((int) $periodEnd->format('N')) === 7;
+	}
+
 	// ========== Mois ==========
 
 	/**
@@ -212,6 +222,16 @@ class DatePeriod
 	public static function isFullMonth(\DateTime $periodStart, \DateTime $periodEnd): bool
 	{
 		return $periodStart->format('Ym') === $periodEnd->format('Ym') && ((int) $periodStart->format('d')) === 1 && null !== ($lastDayOfMonth = \Osimatic\Helpers\Calendar\DateTime::getLastDayOfMonth($periodEnd->format('Y'), $periodEnd->format('m'))) && $lastDayOfMonth->format('d') === $periodEnd->format('d');
+	}
+
+	/**
+	 * @param \DateTime $periodStart
+	 * @param \DateTime $periodEnd
+	 * @return bool
+	 */
+	public static function isFullMonths(\DateTime $periodStart, \DateTime $periodEnd): bool
+	{
+		return ((int) $periodStart->format('d')) === 1 && null !== ($lastDayOfMonth = \Osimatic\Helpers\Calendar\DateTime::getLastDayOfMonth($periodEnd->format('Y'), $periodEnd->format('m'))) && $lastDayOfMonth->format('d') === $periodEnd->format('d');
 	}
 
 	// ========== Année ==========
