24ffb1709a144f0e36db6bb48799e1db2ef8e1c5
Benoit <guiraudou@osimatic.com>
2022-09-28 10:58:13 +0200

    add class user agent

--- src/Network/UserAgent.php
+++ src/Network/UserAgent.php
@@ -30,13 +30,49 @@ class UserAgent
 	public const DEVICE_TYPE_BOT 				= 'bot';
 	public const DEVICE_TYPE_PROJECTOR 			= 'projector';
 
+	public object $parser;
+
+	public string $readableRepresentation;
+
 	public ?string $browserName;
 	public ?string $browserVersion;
 
 	public ?string $osName;
 	public ?string $osVersion;
 
+	public bool $deviceIsMobile;
 	public ?string $deviceType;
 	public ?string $deviceManufacturer;
 	public ?string $deviceModel;
+
+	public function __construct(string $userAgent)
+	{
+		$result = new \WhichBrowser\Parser($userAgent);
+		$this->parser = $result;
+
+		$this->readableRepresentation = $result->toString();
+
+		$this->browserName = $result->browser->getName();
+		$this->browserName = !empty($this->browserName) ? $this->browserName : null;
+		$this->browserVersion = $result->browser->getVersion();
+		$this->browserVersion = !empty($this->browserVersion) ? $this->browserVersion : null;
+
+		$this->osName = $result->os->getName();
+		$this->osName = !empty($this->osName) ? $this->osName : null;
+		$this->osVersion = $result->os->getVersion();
+		$this->osVersion = !empty($this->osVersion) ? $this->osVersion : null;
+
+		$this->deviceIsMobile = $result->isMobile();
+		$this->deviceType = $result->device->type;
+		$this->deviceType = !empty($this->deviceType) ? $this->deviceType : null;
+		$this->deviceManufacturer = $result->device->getManufacturer();
+		$this->deviceManufacturer = !empty($this->deviceManufacturer) ? $this->deviceManufacturer : null;
+		$this->deviceModel = $result->device->getModel();
+		$this->deviceModel = !empty($this->deviceModel) ? $this->deviceModel : null;
+	}
+
+	public static function parse(string $userAgent): UserAgent
+	{
+		return new UserAgent($userAgent);
+	}
 }
