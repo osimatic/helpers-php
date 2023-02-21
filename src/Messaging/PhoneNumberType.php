f4fc36beddc7e48adb01b734f0e83302a9c7f118
Benoit <guiraudou@osimatic.com>
2023-02-21 22:07:09 +0100

    add type VOIP

--- src/Messaging/PhoneNumberType.php
+++ src/Messaging/PhoneNumberType.php
@@ -9,6 +9,7 @@ enum PhoneNumberType: int
 	case PREMIUM_RATE 	= \libphonenumber\PhoneNumberType::PREMIUM_RATE;
 	case TOLL_FREE 		= \libphonenumber\PhoneNumberType::TOLL_FREE;
 	case SHARED_COST 	= \libphonenumber\PhoneNumberType::SHARED_COST;
+	case VOIP 			= \libphonenumber\PhoneNumberType::VOIP;
 	case UNKNOWN 		= \libphonenumber\PhoneNumberType::UNKNOWN;
 
 
