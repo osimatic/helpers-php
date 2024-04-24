7c94e296307cb20d20ddfd5f5e1144ecadcea9c7
Benoit <guiraudou@osimatic.com>
2024-04-24 15:56:33 +0200

    add functions parse

--- src/Route/TransitTravelMode.php
+++ src/Route/TransitTravelMode.php
@@ -8,4 +8,28 @@ enum TransitTravelMode: string
 	case SUBWAY = 'SUBWAY';
 	case TRAIN = 'TRAIN';
 	case LIGHT_RAIL = 'LIGHT_RAIL';
+
+	/**
+	 * @param string|null $type
+	 * @return TransitTravelMode|null
+	 */
+	public static function parse(?string $type): ?self
+	{
+		if (null === $type) {
+			return null;
+		}
+
+		$type = mb_strtoupper($type);
+
+		if ('TRAM' === $type || 'LIGHT_SUBWAY' === $type) {
+			return self::LIGHT_RAIL;
+		}
+		if ('METRO' === $type) {
+			return self::SUBWAY;
+		}
+		if ('RAIL' === $type) {
+			return self::TRAIN;
+		}
+		return self::tryFrom($type);
+	}
 }
