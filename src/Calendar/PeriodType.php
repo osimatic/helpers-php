792320db52c5d647bcfd2d791ecc6fbed62b2c2f
Benoit <guiraudou@osimatic.com>
2024-09-09 15:21:01 +0200

    add DAY and MINUTE on PeriodType

--- src/Calendar/PeriodType.php
+++ src/Calendar/PeriodType.php
@@ -4,7 +4,9 @@ namespace Osimatic\Calendar;
 
 enum PeriodType: string
 {
+	case MINUTE = 'minute';
 	case HOUR = 'hour';
+	case DAY = 'day';
 	case DAY_OF_WEEK = 'day_of_week';
 	case DAY_OF_MONTH = 'day_of_month';
 	case WEEK = 'week';
