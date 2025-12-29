50e942bdc648b5062e3cfa88fc57b26d53074a60
Benoit <guiraudou@osimatic.com>
2025-12-29 18:11:59 +0100

    add function getBool

--- src/Data/Input.php
+++ src/Data/Input.php
@@ -8,6 +8,7 @@ use Symfony\Component\Console\Input\InputInterface;
 class Input
 {
 	/**
+	 * Récupération des valeurs selon le contexte (HTTP ou CLI)
 	 * @param Request|InputInterface $input
 	 * @param string $key
 	 * @return mixed|null
@@ -30,5 +31,28 @@ class Input
 		}
 
 		return null;
+	}
+
+	public static function getBool(Request|InputInterface $input, string $key, bool $default=false): bool
+	{
+		return self::toBool(self::get($input, $key, $default));
+	}
+
+	private static function toBool(mixed $value, bool $default=false): bool
+	{
+		if ($value === null) {
+			return $default;
+		}
+		if (\is_bool($value)) {
+			return $value;
+		}
+		$v = strtolower((string) $value);
+		if ($v === '1' || $v === 'true' || $v === 'yes' || $v === 'on') {
+			return true;
+		}
+		if ($v === '0' || $v === 'false' || $v === 'no' || $v === 'off') {
+			return false;
+		}
+		return $default;
 	}
 }
